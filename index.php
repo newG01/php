2ab3f74460a97b7df94a8525a328d0a9517972ad
fecigin <19605929+newG01@users.noreply.github.com>
2017-08-28 22:30:32 -0700

    Update index.php

--- index.php
+++ index.php
@@ -14,5 +14,11 @@ echo "Hello there";
   <input type="text" name="lname">
   <input type="submit">
 </form>
+  <?php
+   if (empty($_POST["fname"])) {
+    $emailErr = "first name is required";
+  }
+  echo "you POST : ".POST['fname'];
+  ?>
 </body>
 </html>
