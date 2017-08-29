38d265d4a954f2c835122583a61aadf322ed6cd8
fecigin <rizalahmad0012@gmail.com>
2017-08-28 22:15:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
 echo "Hello there";
 
 ?>
+
+<html>
+  <head>
+    <title>POST php</title>
+  </head>
+<body>
+<form>
+  <input type="text" name="fname">
+  <input type="text" name="lname">
+  <input type="submit">
+</form>
+</body>
+</html>
